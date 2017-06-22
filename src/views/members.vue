1b0c339a0524cc6483322a3d1e60e85d74d6b343
pollux <pollux@polluxMac.local>
2017-06-22 17:45:21 +0800

    添加部分实例

--- src/views/members.vue
+++ src/views/members.vue
@@ -1,6 +1,11 @@
 <template>
-    <div>人员管理</div>
+    <Tabs>
+        <Tab-pane label="macOS" icon="social-apple">标签一的内容</Tab-pane>
+        <Tab-pane label="Windows" icon="social-windows">标签二的内容</Tab-pane>
+        <Tab-pane label="Linux" icon="social-tux">标签三的内容</Tab-pane>
+    </Tabs>
 </template>
+
 <script>
     export default {
         mounted (){
